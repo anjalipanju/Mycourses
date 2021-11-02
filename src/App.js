e54d86ea1c5de19443fc052f399bf390af50ca45
Anjali P <45040570+anjalipanju@users.noreply.github.com>
2021-11-02 13:11:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Courses from './components/Courses';
+import Add from './components/Add';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Add/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
